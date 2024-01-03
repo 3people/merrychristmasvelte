835e97a017d655d3e06c526ef33eec1cbbf8dc85
3people <smsn7031@naver.com>
2024-01-03 18:02:18 +0900

    fix: Day 2

--- src/routes/day-2/+page.svelte
+++ src/routes/day-2/+page.svelte
@@ -1,7 +1,34 @@
 <script lang="ts">
-	import Counter from './Counter.svelte'
+	import Cookie from './Cookie.svelte'
+	import { fly } from 'svelte/transition'
+
+	let count = 0
+
+	const increment = () => {
+		count += 1
+	}
+
+	const reset = () => {
+		count = 0
+	}
 </script>
 
-<div class="flex h-full w-full items-center justify-center bg-gray-800">
-	<Counter />
+<div
+	class="flex h-full w-full flex-col items-center justify-center bg-gray-800"
+>
+	{#key count}
+		<Cookie on:click={increment} />
+	{/key}
+	<div class="h-16 w-16">
+		{#key count}
+			<div
+				in:fly={{ y: 50, duration: 100 }}
+				out:fly={{ y: -50, duration: 100 }}
+				class="select-none text-center text-3xl text-white"
+			>
+				{count}
+			</div>
+		{/key}
+	</div>
+	<button class="mt-10 text-2xl text-white" on:click={reset}>reset</button>
 </div>
