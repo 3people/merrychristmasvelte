704d12c80ad9d407fd7d34256e8c6764b5e5f5e8
3people <smsn7031@naver.com>
2024-01-04 17:34:59 +0900

    design: set default background color

--- src/routes/day-2/+page.svelte
+++ src/routes/day-2/+page.svelte
@@ -13,9 +13,7 @@
 	}
 </script>
 
-<div
-	class="flex h-full w-full flex-col items-center justify-center bg-gray-800"
->
+<div class="flex h-full w-full flex-col items-center justify-center">
 	{#key count}
 		<Cookie on:click={increment} />
 	{/key}
@@ -24,11 +22,11 @@
 			<div
 				in:fly={{ y: 50, duration: 100 }}
 				out:fly={{ y: -50, duration: 100 }}
-				class="select-none text-center text-3xl text-white"
+				class="select-none text-center text-3xl"
 			>
 				{count}
 			</div>
 		{/key}
 	</div>
-	<button class="mt-10 text-2xl text-white" on:click={reset}>reset</button>
+	<button class="mt-10 select-none text-2xl" on:click={reset}>reset</button>
 </div>
